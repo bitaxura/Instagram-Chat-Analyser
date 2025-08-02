b1a6f60b3ab87d8404f4df9de53fb4f8844b4981
Daksh <jdaksh1322@gmail.com>
2025-08-02 16:59:27 -0400

    dataset is properly ccleaned and put into individual single lists

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,23 +1,45 @@
 import os
 import ast
+import regex as re
 import pandas as pd
 import kagglehub
 
-def flatten_df(df: pd.DataFrame) -> pd.DataFrame:
-    single_speech = []
-    for _, row in df.iterrows():
-        dialog = ast.literal_eval(row['dialog'])
-        emotion = list(map(int, row['emotion'].strip('[]').split()))
-        for text, emo in zip(dialog, emotion):
-            single_speech.append({"text": text.strip(), "label": emo})
-    return pd.DataFrame(single_speech)
-
+#pd.set_option('display.max_colwidth', None)
 path = kagglehub.dataset_download("thedevastator/dailydialog-unlock-the-conversation-potential-in")
 
 train_df = pd.read_csv(os.path.join(path, "train.csv"))
 valid_df = pd.read_csv(os.path.join(path, "validation.csv"))
 test_df  = pd.read_csv(os.path.join(path, "test.csv"))
 
-train_single_speech_df = flatten_df(train_df)
-valid_single_speech_df = flatten_df(valid_df)
-test_single_speech_df  = flatten_df(test_df)
+def split_utterances(cell: list) -> list[str]:
+    if isinstance(cell, list) and len(cell) == 1 and isinstance(cell[0], str):
+        text = cell[0].strip()
+
+        utterances = re.split(r'\s{2,}', text)
+
+        return [utt.strip() for utt in utterances if utt.strip()]
+
+    return []
+
+def pre_process_df(df: pd.DataFrame) -> tuple[list[list[str]], list[list[int]]]:
+    df['dialog'] = df['dialog'].apply(ast.literal_eval)
+    dialogs = df['dialog'].apply(split_utterances).tolist()
+
+    df['emotion'] = df['emotion'].apply(lambda cell: [int(x) for x in cell.strip('[]').split()])
+    emotions = df['emotion'].tolist()
+
+    return dialogs, emotions
+
+def process_df(df: pd.DataFrame) -> tuple[list[str], list[int]]:
+    dialogs, emotions = pre_process_df(df)
+    all_dialogs = []
+    all_emotions = []
+    for dialog, emotion in zip(dialogs, emotions):
+        for text, emo in zip(dialog, emotion):
+            all_dialogs.append(text)
+            all_emotions.append(emo)
+    return all_dialogs, all_emotions
+
+train_dialogs, train_emotions = process_df(train_df)
+valid_dialogs, valid_emotions = process_df(valid_df)
+test_dialogs, test_emotions = process_df(test_df)
